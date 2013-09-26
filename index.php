1a4711cd52c00676aa17398f637d3e4abaa5dcdf
sarahkonyer <skonyer@gmail.com>
2013-09-26 10:44:29 -0700

    Wordpress child theme

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Master Template. This template will be used to display your blog posts and pages if page.php does not exist.
+ *
+ * @package blm_basic
+ */
+
+get_header(); ?>
+
+<div id="main">
+	
+	<section id="content">
+		
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			
+			<?php the_content( __( 'Read more', 'blm_basic' ) ); ?>
+
+			<?php
+				$meta_quote = get_post_meta( $post->ID, "quotation", true );
+				if ($meta_quote) {
+					echo "<p>Quote: <strong>".$meta_quote."</strong></p>";
+				} else {
+					echo "<p>No quote found.</p>";
+				}
+				?>
+				
+			<?php get_template_part( 'inc/meta' ); ?>
+
+		</article>
+		
+	<?php endwhile; endif; ?>
+	
+	<?php get_template_part( 'inc/nav' ); ?>
+	
+	</section><!-- #content -->
+
+<?php get_sidebar(); ?>
+
+</div><!-- #main -->
+
+<?php get_footer(); ?>
